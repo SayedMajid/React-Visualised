ee1b701ae09f102b240d15c82108e900598fff86
Majid Sayyed <sydmajid04@gmail.com>
2023-02-24 23:10:12 +0530

    My first class

--- HandRolledReact/src/index.js
+++ HandRolledReact/src/index.js
@@ -3,11 +3,37 @@ import ReactDOM from "react-dom/client";
 
 const reactRoot = ReactDOM.createRoot(document.querySelector("#reactRoot"));
 
+const user = { name: "Dan Abramov" };
+
+const styles = {
+  border: "1px solid black",
+  fontFamily: "sans-serif",
+  color: "teal",
+  textAlign: "center",
+};
+
+class Person extends React.Component {
+  constructor(props) {
+    super(props); //* this.props = props
+  }
+
+  render() {
+    return (
+      <>
+        <h1>
+          Hello from {this.props.name}. ID {this.props.id}
+        </h1>
+      </>
+    );
+  }
+}
+
 const div = (
-  <div>
-    <h1>Hello JSX in Handrolled</h1>
-    <input />
-    <button>Click Here...</button>
+  <div style={styles}>
+    <h1>Hello, JSX Handrolled!!!</h1>
+    <div>
+      <Person id={Math.random(0, 9)} name="Dan Abramov" />
+    </div>
   </div>
 );
 
