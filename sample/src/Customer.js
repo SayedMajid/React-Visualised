9cb91cb1a0f6a86688150cfc58f770afb1e0a71a
Majid Sayyed <sydmajid04@gmail.com>
2023-02-28 23:10:00 +0530

    Lifecycle methods

--- sample/src/Customer.js
+++ sample/src/Customer.js
@@ -1,11 +1,64 @@
 import PropTypes from "prop-types";
+import React from "react";
 
-export default function Customer(props) {
-  return (
-    <h5>
-      {props.cust.id} {props.cust.name} {props.cust.address}
-    </h5>
-  );
+export default class Customer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      time: new Date(),
+    };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment() {
+    this.setState({ ...this.state, counter: this.state.counter + 1 });
+  }
+
+  decrement() {
+    this.setState({ ...this.state, counter: this.state.counter - 1 });
+  }
+
+  static getDerivedStateFromProps() {
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("Component did mount called");
+  }
+
+  shouldComponentUpdate() {
+    console.log("Should component update called");
+    if (this.state.counter % 2 === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log("Get snapshot before updarte");
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log(`Component did update ?? ${this.state.counter}`);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Managing State in React Component</h1>
+        <div>
+          <h3>Counter is {this.state.counter} </h3>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <h3>Time is {this.state.time.toDateString()} </h3>
+        </div>
+      </>
+    );
+  }
 }
 
 Customer.propTypes = {
